f1d3a6d5272ee4d87a0d5fa7203604e0e93b6727
jl-costa <juan@conseev.com>
2018-05-01 17:13:38 +0100

    add bids_per_project

--- get_bids_per_project.py
+++ get_bids_per_project.py
@@ -20,19 +20,16 @@ from freelancer_sdk_v2.resources.projects.exceptions import \
 token = '***REMOVED***'
 session = Session(oauth_token=token)
 
-# Function to get 100 successful (awarded) bids for a given user
-def get_successful_bids_per_user(user_id, offset):
+# Function to get 100 bids for a given project
+def get_bids_per_project(project_id, offset):
     url = os.environ.get('FLN_URL')
     oauth_token = os.environ.get('FLN_OAUTH_TOKEN')
     session = Session(oauth_token=token, url=url)
     bids_data = pd.DataFrame()
 
     get_bids_data = {
-        'user_ids': [
-            user_id
-        ],
-        'awarded_statuses': [
-            'awarded'
+        'project_ids': [
+            project_id
         ],
         'offset': offset,
         'project_details': True
@@ -47,35 +44,23 @@ def get_successful_bids_per_user(user_id, offset):
         else:
             return bids_data
     except BidsNotFoundException:
-        return bids_data        
+        return bids_data
 
-# Function to parse all successful bids for a given user
-def parse_all_successful_bids_per_user(userid, offset):
+# Function to parse all bids for a given project
+def parse_all_bids_per_project(project_id, offset):
     all_bids = pd.DataFrame()
-    bids_data = get_successful_bids_per_user(userid, offset)
+    bids_data = get_bids_per_project(project_id, offset)
     
     while bids_data.empty == False:
         all_bids = all_bids.append(bids_data)
         offset += 100
-        bids_data = get_successful_bids_per_user(userid, offset)
-        # print(userid)
+        bids_data = get_bids_per_project(project_id, offset)
+        # print(project_id)
         # print(all_bids.min()['time_submitted'])
         # print(offset)
         # print("len of all_bids is {}".format(len(all_bids)))
 
     return all_bids
-       
-        
-    #     try:
-            
-    #         all_user_bids_df = parse_all_successful_bids_per_user(userid, offset, all_user_bids_df)
-    #         return all_user_bids_df
-    #     except IndexError:
-    #         return all_user_bids_df
-    # else:
-    #     return all_user_bids_df
-
-
 
 # Initialize local SQL connection
 cnx = create_engine('mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(db_config['user'], 
@@ -85,34 +70,35 @@ cnx = create_engine('mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(db_c
                                                                             db_config['db']), 
                                                                             echo=False)
 
-# Import 100 user IDs whose bids still have to be parsed. Select only freelancer or hybrid profiles.
-ids_result = cnx.execute("SELECT userid FROM users WHERE (chosen_role = 'freelancer' OR chosen_role = 'both') AND userid NOT IN (SELECT * FROM processed_ids) LIMIT 50")
+# Import 100 project IDs whose bids still have to be parsed.
+# ids_result = cnx.execute("SELECT project_id FROM successful_bids WHERE project_id NOT IN (SELECT * FROM processed_projects_bids) LIMIT 50")
+ids_result = cnx.execute("SELECT project_id FROM successful_bids LIMIT 10")
 
 # Initialize empty list
-list_of_ids = []
+list_of_project_ids = []
 
 # Iterate through output of SQL query, and add IDs to list
 for row in ids_result:
-     list_of_ids.append(row['userid'])
+     list_of_project_ids.append(row['project_id'])
 
-# Create queue of users from list of IDs
-user_id_queue = deque(list_of_ids)
+# Create queue of projects from list of IDs
+project_id_queue = deque(list_of_project_ids)
 
 # Initialize empty dataframe
-successful_bids_df = pd.DataFrame()
+all_bids_df = pd.DataFrame()
 
 # Initialize offset
 offset = 0
 
-# Parse user_ids until queue is empty
-while user_id_queue:
-    userid = user_id_queue.pop()
-    bids_df = parse_all_successful_bids_per_user(userid, offset)
+# Parse project_ids until queue is empty
+while project_id_queue:
+    project_id = project_id_queue.pop()
+    bids_df = parse_all_bids_per_project(project_id, offset)
     
     # Process data returned by API, if bids were found
     if bids_df.empty != True:
         bids_df = bids_df.loc[:,~bids_df.columns.duplicated()]
-        successful_bids_df = successful_bids_df.append(bids_df)
+        all_bids_df = all_bids_df.append(bids_df)
     
     # If bids weren't found, do nothing
     else:
@@ -120,14 +106,13 @@ while user_id_queue:
 
     time.sleep(3)
 
-# Store user_ids that were processed in dedicated table
-
-pd.Series(list_of_ids).to_sql(name='processed_ids', con=cnx, if_exists = 'append', index=False)
+# Store project_ids that were processed in dedicated table
+pd.Series(list_of_project_ids).to_sql(name='processed_projects_bids', con=cnx, if_exists = 'append', index=False)
 
 # Store bids in DB if bids DF isn't empty
 
-if successful_bids_df.empty != True:
-    successful_bids_df.to_sql(name='successful_bids', con=cnx, if_exists = 'append', index=False)
+if all_bids_df.empty != True:
+    all_bids_df.to_sql(name='all_bids', con=cnx, if_exists = 'append', index=False)
 
 # Print output
-print("{} user_ids processed with {} successful bids found at {}".format(len(list_of_ids), len(successful_bids_df), datetime.now()))
+print("{} project_ids processed with {} bids found at {}".format(len(list_of_project_ids), len(all_bids_df), datetime.now()))
