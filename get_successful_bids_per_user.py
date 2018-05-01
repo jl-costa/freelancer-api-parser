5fbd802962b2622365a50139c1383408b7aeff7a
jl-costa <juan@conseev.com>
2018-05-01 11:25:35 +0100

    new version of bids

--- get_successful_bids_per_user.py
+++ get_successful_bids_per_user.py
@@ -86,8 +86,7 @@ cnx = create_engine('mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(db_c
                                                                             echo=False)
 
 # Import 100 user IDs whose bids still have to be parsed. Select only freelancer or hybrid profiles.
-# ids_result = cnx.execute("SELECT userid FROM users WHERE (chosen_role = 'freelancer' OR chosen_role = 'both') AND userid NOT IN (SELECT userid FROM bids) LIMIT 100")
-ids_result = cnx.execute("SELECT userid FROM users WHERE (chosen_role = 'freelancer' OR chosen_role = 'both') LIMIT 15")
+ids_result = cnx.execute("SELECT userid FROM users WHERE (chosen_role = 'freelancer' OR chosen_role = 'both') AND userid NOT IN (SELECT * FROM processed_ids) LIMIT 50")
 
 # Initialize empty list
 list_of_ids = []
@@ -119,7 +118,7 @@ while user_id_queue:
     else:
         pass
 
-    time.sleep(0.1)
+    time.sleep(1)
 
 # Store user_ids that were processed in dedicated table
 
