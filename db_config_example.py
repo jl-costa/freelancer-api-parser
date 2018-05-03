57d4b3237ab84a56185ae48e7ed130cb5278d244
root <root@juan-server1.com>
2018-05-03 16:09:23 +0000

    added db_config_example

---
+++ db_config_example.py
@@ -0,0 +1,7 @@
+db_config = {
+'host':'', #typically localhost if running this locally
+'port':'', #typically 3306 if running a standard MySQl engine
+'user':'', #mysql user with full privileges on the DB
+'pass':'', #password of the mysql user
+'db':''} #database in which all tables will be stored (must be already created)
+
